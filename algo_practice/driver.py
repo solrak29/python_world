9c2f113b01f8b37704234b18964f5356bbe9e12d
Carlos Negron <carlos.negron@bestow.com>
2022-06-21 21:18:02 -0400

    Adding tiny opt for selection sort

--- algo_practice/driver.py
+++ algo_practice/driver.py
@@ -1,6 +1,10 @@
 from selection import selection_sort
+from selection_opt import selection_sort as sort_opt
 
 if __name__ == "__main__":
     value = [5,2,3,1,8,7]
     selection_sort(value)
+    print(value)
+    value = [5,2,3,1,8,7]
+    sort_opt(value)
     print(value)
---
+++ algo_practice/selection_opt.py
@@ -0,0 +1,12 @@
+
+def selection_sort(a:[int] ):
+    print(f'selection sort of {a}')
+    for idx, val in enumerate(a):
+        minIndex = idx
+        for index in range(minIndex+1, len(a)):     
+            if a[minIndex] > a[index]:
+                a[minIndex] = a[index]
+                minIndex = index
+        tmp = a[idx] 
+        a[idx] = a[minIndex]
+        a[minIndex] = tmp
