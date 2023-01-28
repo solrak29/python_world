13d61a6351de8f10da722a0710cf6beee2168553
Carlos <solrak29@yahoo.com>
2023-01-27 23:19:22 -0500

    Fixed dont odds, more testing needed

--- craps/test_pass_dont_switch.py
+++ craps/test_pass_dont_switch.py
@@ -1,6 +1,7 @@
 from pass_dont_pass_switch import PassDontSwitch 
 
-def test_dont_with_odds():
+
+def test_dont_with_odds_4():
     """
     Starting with 500 bank roll, 5 min bet with 10X odds.
     """
@@ -31,3 +32,36 @@ def test_dont_with_odds():
     assert p5.odds_bet == 20
     p5.out(7)  # this is ok since point is made
     assert p5.end_balance == 535 # 20 + 10
+
+
+def test_dont_with_odds_5():
+    """
+    Starting with 500 bank roll, 5 min bet with 10X odds.
+    """
+    p5 = PassDontSwitch(500, 5, 10)
+
+    # Lose 3 times to get into the don't mode of betting
+    p5.out(7) # 495
+    p5.out(7) # 490
+    p5.out(7) # 485
+    assert p5.dont == True
+
+    # Get our winnings to where we will play the odds.
+    p5.craps(3) # 490
+    p5.craps(3) # 500
+    p5.craps(3)
+    p5.craps(3)
+    p5.craps(3)
+    p5.craps(3)
+    p5.craps(3) # 520
+
+    p5.point_made(5)
+    #  Odd are 2 to 1 you 20 to play odds with.
+    #  Odds calculated should be 20 
+    p5.roll(3)  # this is ok since point is made
+    p5.roll(5)  # this is ok since point is made
+    p5.roll(5)  # this is ok since point is made
+    p5.roll(5)  # this is ok since point is made
+    assert p5.odds_bet == 18  # becasue the odds are 3:2 we either can do 18 or 21 for odds.
+    p5.out(7)  # this is ok since point is made
+    assert p5.end_balance == 555 # 20 + 10
